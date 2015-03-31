9916ea1909ca78afbf6ed3331d12e65a5a527c59
123Evelyn <evelyn.lumi@gmail.com>
2015-03-31 11:43:30 +0300

    proov

---
+++ Kolmkola.java
@@ -0,0 +1,18 @@
+public class Kolmkola {
+        int pohitoon;
+		int terts;
+		int kvint;
+		
+		public Kolmkola (int p){
+			pohitoon=p;
+			terts = p + 4;
+			kvint=p + 7;
+			
+//			System.print.out 
+		}
+		int kysiKvint(){return kvint;}
+	//	Return format {
+			
+		//}
+		int kysiTerts () {return terts;}
+}
---
+++ andmed.java
@@ -0,0 +1,6 @@
+class Andmed
+    public static void main (String)[] args) {
+	       Kolmkola k=new Kolmkola(65);
+		   System.out.println(k.kysiKvint());
+		   System.out.println (k.kysiTerts ());
+	}
