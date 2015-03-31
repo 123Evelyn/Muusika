ea21e62097ae7a7b247242a7cb503ce2d1244029
123Evelyn <evelyn.lumi@gmail.com>
2015-03-31 11:42:30 +0300

    proov

---
+++ proov1.java
@@ -0,0 +1,8 @@
+
+
+public class proov1 {
+       public static void main (String[] args) {
+	       Kolmkola k = new Kolmkola (62);
+		   System.out.println (k.pohitoon+" "+k.kysiTerts()+ " "+k.kysiKvint());
+	}
+}
